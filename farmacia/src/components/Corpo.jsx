c015a63eeb7d34ba550f01c6694ae623e9e30f1f
isac-barbosa <santosisac196@gmail.com>
2025-09-02 14:29:31 -0300

    farmarcia

--- farmacia/src/components/Corpo.jsx
+++ farmacia/src/components/Corpo.jsx
@@ -13,7 +13,6 @@ function Corpo(){
             <Titulo  titulo = {"SUA FARMÁRCIA"}/>
             </div>
             <div className='conteudo-container'>
-
             <Texto className = "texto" textocriado = {"Márcia possui formação em administração de empresas, com uma paixão especial por empreendedorismo e design Márcia sempre foi uma pessoa determinada e criativa. Ela cresceu em uma família onde o empreendedorismo era valorizado, inspirada pelo sucesso de seu tio Mário, que administrava uma farmácia local. Depois de se formar na faculdade, Márcia se casou e começou a trabalhar em uma empresa de marketing, onde pôde desenvolver suas habilidades em design e gestão. No entanto, sua vida pessoal deu uma reviravolta quando seu casamento chegou ao fim após um evento no mínimo estranho. Seu marido saiu para comprar cigarros e nunca mais voltou, deixando Márcia sozinha para lidar com a dor do abandono e o desafio de reconstruir sua vida."} />
             <img src='./img/loja.svg' className='img-container' />
             </div>
--- farmacia/src/components/Footer.jsx
+++ farmacia/src/components/Footer.jsx
@@ -2,10 +2,10 @@ import  './Footer.css'
 function Footer(){
     return(
         <footer className='footer-container'> 
-        <a className='link'> <img src="./img/local.svg" className='icon-container'/>Rodovia José Carlos Daux</a>
+        <a  href = 'https://www.google.com/maps/@-27.5483077,-48.499171,3a,75y,64.52h,79.81t/data=!3m7!1e1!3m5!1sS5t4PY80Y4K2h78soVXulg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D10.185303167986447%26panoid%3DS5t4PY80Y4K2h78soVXulg%26yaw%3D64.51630427865287!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI1MDgyNS4wIKXMDSoASAFQAw%3D%3D' target='blank' className='link'> <img src="./img/local.svg" className='icon-container'/>Rodovia José Carlos Daux</a>
         <a className='direitos'>  © 2025 Sua Farmárcia - Todos os direitos reservados</a>
         <a href="https://github.com/isac-barbosa" className='link'> <img src='./img/github.svg' /> Isac Barbosa</a>
-        
+        <a href="http://" target="_blank" rel="noopener noreferrer"></a>
         
         </footer>
     )
